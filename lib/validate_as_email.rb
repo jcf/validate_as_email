4483849cebc3d2aa2ba0a333d26af0539e190362
James Conroy-Finn <james@logi.cl>
2012-07-20 13:18:53 +0100

    Add custom RSpec matcher

--- lib/validate_as_email.rb
+++ lib/validate_as_email.rb
@@ -1,3 +1,7 @@
 require 'validate_as_email/version'
 require 'active_model'
 require 'active_model/validations/email_validator'
+
+if defined?(RSpec)
+  require 'validate_as_email/rspec'
+end
---
+++ lib/validate_as_email/rspec.rb
@@ -0,0 +1,48 @@
+require 'rspec/rails/extensions/active_record/base'
+# Adds a custom matcher to RSpec to make it easier to make sure your email
+# column is valid.
+#
+#   class Person
+#     include ActiveModel::Validations
+#
+#     validates_as_email :email
+#
+#     def initialize(attributes = {})
+#       @attributes = attributes
+#     end
+#
+#     def email
+#       @attributes[:email]
+#     end
+#
+#     def email=(address)
+#       @attributes[:email] = address
+#     end
+#   end
+#
+#   describe Person
+#     it { should have_a_valid_email_address_for(:email) }
+#   end
+RSpec::Matchers.define(:have_a_valid_email_address_for) do |column_name|
+  match do |klass|
+    %w(
+      matz@example.com
+      a@b.tld
+      1@8.8.8.8
+    ).each do |valid|
+      klass.send("#{column_name}=", valid)
+      klass.should have(0).errors_on(column_name)
+    end
+
+    %w(
+      word
+      a@
+      @b
+      example.com
+      8.8.8.8
+    ).each do |invalid|
+      klass.send("#{column_name}=", invalid)
+      klass.should have(1).error_on(column_name)
+    end
+  end
+end
