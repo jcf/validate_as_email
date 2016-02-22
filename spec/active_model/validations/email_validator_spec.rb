6989dbaead3279abbd77de6d4d1b42084c4c8dbc
James Conroy-Finn <james@logi.cl>
2016-02-22 21:41:16 +0000

    Update tests to be data-driven

--- spec/active_model/validations/email_validator_spec.rb
+++ spec/active_model/validations/email_validator_spec.rb
@@ -1,75 +1,57 @@
 require 'spec_helper'
 
 describe ActiveModel::Validations::EmailValidator do
-  let(:person) { Person.new(email: 'invalid') }
-
-  let(:options) { {attributes: [:email]} }
-
-  def build_validator(options = {})
-    ActiveModel::Validations::EmailValidator.new(
-      options.merge(attributes: [:email])
-    )
-  end
-
-  subject(:validator) { build_validator }
-
   describe '#validate' do
-    context 'with a valid email address' do
-      before do
-        person.email = 'james@logi.cl'
-      end
+    [
+      {
+        desc: 'with a valid email address',
+        validator: {},
+        people: [
+          {email: 'a@b.c'},
+          {email: 'a+x@b.c'},
+          {email: 'a@192.168.0.1'},
+          {email: '1@1.com'}
+        ],
+        errors: []
+      },
+      {
+        desc: 'with nil allowed',
+        validator: {allow_nil: true},
+        people: [ {email: nil} ],
+        errors: []
+      },
+      {
+        desc: 'with blank allowed',
+        validator: {allow_blank: true},
+        people: [ {email: ' '}, {email: "\t\n"} ],
+        errors: []
+      },
+      {
+        desc: 'with no message provided',
+        validator: {},
+        people: [ {email: 'invalid'}, {email: '  '} ],
+        errors: ['Email is invalid']
+      },
+      {
+        desc: 'with a custom error message',
+        validator: {message: 'is kinda odd looking'},
+        people: [ {email: 'invalid'}, {email: '  '} ],
+        errors: ['Email is kinda odd looking']
+      }
+    ].each do |data|
+      data[:people].each do |person_attributes|
+        context data do
+          let(:person) { Person.new(person_attributes) }
 
-      it 'does not add errors' do
-        validator.validate(person)
-        person.errors.to_a.should == []
-      end
-    end
+          let(:validator) do
+            ActiveModel::Validations::EmailValidator.new(
+              data[:validator].merge(attributes: [:email])
+            )
+          end
 
-    context 'with nil allowed' do
-      subject(:validator) do
-        build_validator(allow_nil: true)
-      end
-
-      before do
-        person.email = nil
-      end
-
-      it 'skips adding errors is email is nil' do
-        validator.validate(person)
-        person.errors.to_a.should == []
-      end
-    end
-
-    context 'with blank is allowed' do
-      subject(:validator) do
-        build_validator(allow_blank: true)
-      end
-
-      before do
-        person.email = '  '
-      end
-
-      it 'skips adding errors is email is nil' do
-        validator.validate(person)
-        person.errors.to_a.should == []
-      end
-    end
-
-    context 'with no message provided' do
-      it 'adds a symbol to errors for I18n lookup' do
-        validator.validate(person)
-        person.errors.to_a.should == ['Email is invalid']
-      end
-    end
-
-    context 'with a specific error message provided' do
-      subject(:validator) do
-        build_validator(message: 'is kinda odd looking')
-      end
-
-      it 'uses the message you specify' do
-        validator.validate(person)
-        person.errors.to_a.should == ['Email is kinda odd looking']
+          before { validator.validate(person) }
+          it { expect(person.errors.to_a).to eq(data[:errors]) }
+        end
       end
     end
   end
